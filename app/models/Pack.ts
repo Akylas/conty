c1cce58b47d3e6f058118a7375293168833b915e
farfromrefug <dev@akylas.fr>
2024-10-01 21:54:51 +0200

    chore: prepare Telmi integration

--- app/models/Pack.ts
+++ app/models/Pack.ts
@@ -1,5 +1,7 @@
+import { lc } from '@nativescript-community/l';
 import { Folder, ImageCache, ImageSource, Observable, Utils, path } from '@nativescript/core';
 import { DocumentsService, PackUpdatedEventData, getFileTextContentFromPackFile } from '~/services/documents';
+import { getAudioDuration } from '~/utils';
 import { EVENT_PACK_UPDATED } from '~/utils/constants';
 
 export interface RemoteContentProvider {
@@ -81,12 +83,75 @@ export interface Stage {
     duration?: number;
 }
 
+interface TelmiNodes {
+    startAction: TelmiStartAction;
+    inventory: TelmiInventory[];
+    stages: TelmiStages;
+    actions: TelmiActions;
+}
+
+interface TelmiAction {
+    stage: string;
+    conditions?: TelmiCondition[];
+}
+interface TelmiActions {
+    [k: string]: TelmiAction[];
+    backChildAction: any[];
+}
+
+interface TelmiCondition {
+    comparator: number;
+    item: number;
+    number: number;
+}
+
+interface TelmiStage {
+    image?: string;
+    audio?: string;
+    ok?: TelmiStartAction;
+    home: TelmiStartAction;
+    control?: TelmiControl;
+    items?: TelmiItem[];
+}
+
+interface TelmiStages {
+    [k: string]: TelmiStage;
+    backStage: TelmiStage;
+}
+
+interface TelmiItem {
+    type: number;
+    item: number;
+    number: number;
+}
+
+interface TelmiControl {
+    ok: boolean;
+    home: boolean;
+    autoplay: boolean;
+}
+
+interface TelmiInventory {
+    name: string;
+    initialNumber: number;
+    maxNumber: number;
+    display: number;
+    image: string;
+}
+
+interface TelmiStartAction {
+    action: string;
+    index: number;
+}
+
 export interface IPack {
     id: string;
     title?: string;
     tags: string[];
     thumbnail?: string;
     description?: string;
+    category?: string;
+    pack?: string;
     keywords?: string;
     subtitle?: string;
     age?: number;
@@ -99,6 +164,7 @@ export interface StoryJSON extends PackMetadata {
     stageNodes: Stage[];
     actionNodes: Action[];
 }
+export interface TelmiStoryJSON extends PackMetadata, TelmiNodes {}
 
 let documentsService: DocumentsService;
 
@@ -126,6 +192,8 @@ export function stageCanGoHome(s: Stage) {
     return s && !!s.controlSettings?.home && !!s.homeTransition;
 }
 
+export const ignoreStageNameRegex = new RegExp('\\s*(story|stage)[\\s-_]*(node|title)\\s*', 'i');
+
 export interface Story {
     id: string;
     pack: Pack;
@@ -137,13 +205,15 @@ export interface Story {
     duration: number;
 }
 
-export class Pack extends Observable implements IPack {
+export abstract class Pack extends Observable implements IPack {
     importedDate: number;
     createdDate: number;
     modifiedDate: number;
     title?: string;
     tags: string[];
     thumbnail?: string;
+    category?: string;
+    type?: string;
     keywords?: string;
     subtitle?: string;
     description?: string;
@@ -158,10 +228,13 @@ export class Pack extends Observable implements IPack {
     }
 
     static fromJSON(jsonObj: Pack) {
-        // DEV_LOG && console.log('OCRDocument', 'fromJSON', JSON.stringify(jsonObj));
-        const doc = new Pack(jsonObj.id);
-        Object.assign(doc, jsonObj);
-        return doc;
+        if (jsonObj.type === 'telmi') {
+            return TelmiPack.fromJSON(jsonObj);
+        }
+        return LuniiPack.fromJSON(jsonObj);
+        // const doc = new Pack(jsonObj.id);
+        // Object.assign(doc, jsonObj);
+        // return doc;
     }
 
     get folderPath() {
@@ -171,12 +244,12 @@ export class Pack extends Observable implements IPack {
         return documentsService.dataFolder.getFile(this.id + '.zip').path;
     }
 
-    async getData() {
+    async getData<T = StoryJSON>() {
         const storyJSON = JSON.parse(await getFileTextContentFromPackFile(this.compressed ? this.zipPath : this.folderPath.path, 'story.json', this.compressed === 1 ? true : false)) as StoryJSON;
         return {
             stageNodes: storyJSON.stageNodes,
             actionNodes: storyJSON.actionNodes
-        };
+        } as T;
     }
     getAudio(audio: string): string {
         if (audio) {
@@ -279,5 +352,186 @@ export class Pack extends Observable implements IPack {
 
     toJSONObject() {
         return JSON.parse(this.toString());
+    }
+
+    async findAllStories() {
+        const data = await this.getData();
+        const currentStage = data.stageNodes.find((s) => s.squareOne === true);
+        const storiesStages = this.findStoriesFromStage(data.actionNodes, data.stageNodes, currentStage, []);
+        const result = await Promise.all(
+            storiesStages.map(async (s, index) => {
+                const images = [];
+                const names = [];
+                const audioFiles = [];
+                let durations = [];
+                // DEV_LOG &&
+                //     console.log(
+                //         'storiesStages',
+                //         s.map((s2) => s2.uuid)
+                //     );
+                const stages = s.reduce((acc, stage) => {
+                    if (stageIsStory(stage)) {
+                        audioFiles.push(this.getAudio(stage.audio));
+                        durations.push(stage.duration);
+                        acc.push(stage);
+                    } else if (stageIsOptionStage(data.actionNodes, data.stageNodes, stage)) {
+                        if (stage.image) {
+                            images.push(stage.image);
+                        }
+                        if (stage.name) {
+                            names.push(cleanupStageName(stage));
+                        }
+                    }
+                    return acc;
+                }, [] as Stage[]);
+                if (stages.length === 0) {
+                    return;
+                }
+                durations = await Promise.all(audioFiles.map((s, index) => durations[index] || getAudioDuration(s)));
+                const duration = durations.reduce((acc, v) => acc + v, 0);
+                const hasMultipleChoices = names.length > 1;
+                const lastStage = stages[stages.length - 1];
+                const name =
+                    (lastStage.name && !ignoreStageNameRegex.test(lastStage.name) && cleanupStageName(lastStage)) || (hasMultipleChoices ? undefined : names[0]) || lc('story') + ' ' + (index + 1);
+                // DEV_LOG && console.log('adding story', name, audioFiles, images, names, durations, duration);
+                return {
+                    id: this.id + '_' + index,
+                    pack: this,
+                    stages,
+                    name,
+                    audioFiles,
+                    images,
+                    names,
+                    durations,
+                    duration
+                } as Story;
+            })
+        );
+        // DEV_LOG && console.log('findAllStories', storiesStages.length, JSON.stringify(result.map((s) => ({ images: s.images, audioFiles: s.audioFiles, name: s.name }))));
+        return result.filter((s) => !!s);
+    }
+
+    nextStageFrom(actionNodes: Action[], stageNodes: Stage[], stage?: Stage): Stage[] {
+        // DEV_LOG && console.log('nextStageFrom', JSON.stringify(stage));
+        if (!stage?.okTransition) {
+            return [];
+        }
+        return actionNodes.find((a) => a.id === stage.okTransition.actionNode).options.map((n) => stageNodes.find((s) => s.uuid === n));
+    }
+
+    mapOfStagesForOption(stages: Stage[], optionIndex: number = -1): Stage[] {
+        if (optionIndex === -1 || optionIndex >= stages.length) {
+            if (stages.length && stages[0].controlSettings.autoplay) {
+                // running it alone will ensure we play automatically
+                return [stages[0]];
+            }
+            return stages;
+        } else {
+            return [stages[optionIndex]];
+        }
+    }
+
+    getStoryName(actionNodes: Action[], stageNodes: Stage[], stage: Stage) {
+        // DEV_LOG && console.log('getStoryName', stage.uuid, stage.name);
+        if (stage && (!stage.name || ignoreStageNameRegex.test(stage.name))) {
+            const action = actionNodes.find((a) => a.options.indexOf(stage.uuid) !== -1);
+            const beforeStage = stageNodes.find((s) => s.type !== 'story' && s.okTransition?.actionNode === action.id && s.controlSettings.wheel);
+            // DEV_LOG && console.log('getStoryName beforeStage', beforeStage.uuid, beforeStage.name);
+            if (beforeStage && ignoreStageNameRegex.test(beforeStage.name)) {
+                return null;
+            }
+            return cleanupStageName(beforeStage);
+        }
+        return cleanupStageName(stage);
+    }
+
+    findStoryImage(actionNodes: Action[], stageNodes: Stage[], s: Stage) {
+        if (s.image) {
+            return s.image;
+        }
+        const action = actionNodes.find((a) => a.options.indexOf(s.uuid) !== -1);
+        const beforeStage = stageNodes.find((s) => s.type !== 'story' && s.okTransition?.actionNode === action.id && s.controlSettings.wheel);
+        // DEV_LOG && console.log('findStoryImage', s.uuid, beforeStage.uuid, stageIsStory(beforeStage), beforeStage.image, beforeStage.audio);
+        if (!stageIsStory(beforeStage) && beforeStage?.image) {
+            return beforeStage.image;
+        } else {
+            return this.findStoryImage(actionNodes, stageNodes, beforeStage);
+        }
+    }
+    findStoriesFromStage(actionNodes: Action[], stageNodes: Stage[], stage: Stage, storyPath: Stage[]): Stage[][] {
+        const nextStages = this.nextStageFrom(actionNodes, stageNodes, stage) || [];
+
+        const stages = this.mapOfStagesForOption(nextStages);
+        if (stages.some((s) => storyPath.findIndex((s2) => s2.uuid === s.uuid) !== -1)) {
+            // this is the start of a loop lets stop
+            return [storyPath];
+        }
+        // DEV_LOG &&
+        //     console.log(
+        //         'findStoriesFromStage',
+        //         stage.uuid,
+        //         stages.length,
+        //         storyPath.length,
+        //         storyPath.map((s) => s.uuid)
+        //     );
+        const isStory = stageIsStory(stage);
+        return stages.reduce((acc, s) => {
+            const newUuid = s.uuid;
+            if (storyPath.findIndex((s2) => s2.uuid === newUuid) === -1 && (!isStory || !stageIsStory(s))) {
+                // DEV_LOG &&
+                //     console.log(
+                //         'findStoriesFromStage creating new story',
+                //         s.uuid,
+                //         storyPath.map((s) => s.uuid)
+                //     );
+                const newStoryPath = [...storyPath];
+                newStoryPath.push(s);
+                acc.push(...this.findStoriesFromStage(actionNodes, stageNodes, s, newStoryPath));
+                // not done lets continue
+            } else {
+                // story path done
+                acc.push(storyPath);
+            }
+            return acc;
+        }, []);
+    }
+}
+
+export class LuniiPack extends Pack {
+    static fromJSON(jsonObj: IPack) {
+        // DEV_LOG && console.log('OCRDocument', 'fromJSON', JSON.stringify(jsonObj));
+        const doc = new LuniiPack(jsonObj.id);
+        Object.assign(doc, jsonObj);
+        return doc;
+    }
+}
+
+export class TelmiPack extends Pack {
+    static fromJSON(jsonObj: IPack) {
+        // DEV_LOG && console.log('OCRDocument', 'fromJSON', JSON.stringify(jsonObj));
+        const doc = new TelmiPack(jsonObj.id);
+        Object.assign(doc, jsonObj);
+        return doc;
+    }
+
+    override async getData<T = TelmiStoryJSON>() {
+        const metadata = JSON.parse(await getFileTextContentFromPackFile(this.compressed ? this.zipPath : this.folderPath.path, 'metadata.json', this.compressed === 1 ? true : false)) as PackMetadata;
+        const nodes = JSON.parse(await getFileTextContentFromPackFile(this.compressed ? this.zipPath : this.folderPath.path, 'nodes.json', this.compressed === 1 ? true : false)) as TelmiNodes;
+        return {
+            ...metadata,
+            ...nodes
+        } as T;
+    }
+    override getAudio(audio: string): string {
+        if (audio) {
+            if (this.compressed) {
+                return 'zip://' + this.zipPath + '@audios/' + audio;
+            } else {
+                return this.folderPath.getFolder('audios').getFile(audio).path;
+            }
+        }
+    }
+    override async getImage(asset: string) {
+        return this.getImageInternal(asset, 'images');
     }
 }
